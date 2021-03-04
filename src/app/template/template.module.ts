724f65fb4a91be72c844bc72fc53528c690cebcd
Artur Tarcisio <arturtarcisio1@gmail.com>
2021-03-03 21:40:25 -0300

    Navegando nas paginas atraves de routerLinks

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,15 +1,19 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 
-
-
 @NgModule({
-  declarations: [NavbarComponent, SidebarComponent, FooterComponent],
+  declarations: [
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     NavbarComponent,
